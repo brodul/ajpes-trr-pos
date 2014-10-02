69c17218889fe0e9b7a554ed1a20210948e78d2f
Andraz Brodnik <brodul@brodul.org>
2014-10-02 11:33:48 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import sys
+
+from setuptools import setup, find_packages
+
+version = __import__('importer.').get_version()
+
+
+setup(
+    name='ajpes-trr-pos',
+    version=version,
+    url='https://github.com/brodul/ajpes-trr-pos',
+    author='Andraz Brodnik brodul',
+    author_email='brodul@brodul.org',
+    description=('Uvoznik transakcijskih racunov poslovnih subjektov'
+                 ' (prek poizvedbe na AJPES) v relacijske baze'
+                 ),
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'sqlalchemy==0.9'
+    ],
+    scripts=['importer.py'],
+    entry_points={'console_scripts': [
+        'ajpes-importer = importer:execute_from_command_line',
+    ]},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
