f78865d971d4d0cc4942df214d3b5fe9d8384c6c
Andraz Brodnik <brodul@brodul.org>
2014-10-05 21:35:22 +0200

    Initial version of importer

---
+++ importer.py
@@ -0,0 +1,20 @@
+import codecs
+import xml.etree.ElementTree as et
+
+from sqlalchemy import create_engine
+
+from models import (
+    DBSession,
+    Base,
+)
+
+engine = create_engine("postgresql+pg8000://brodul:test@localhost/test")
+__version__ = '0.1.0'
+
+get_version = lambda: __version__
+
+DBSession.remove()
+DBSession.configure(bind=engine)
+
+Base.metadata.bind = engine
+Base.metadata.create_all(engine)
